7c9a98d935e59f0d3e109355c94a7bf2f8485559
Steve Zelaznik <steve.zelaznik@iorahealth.com>
2019-01-18 21:16:37 +0000

    add async-contains-then-clause rule

---
+++ docs/rules/async-contains-then-clause.md
@@ -0,0 +1,19 @@
+# Async functions which contains "Promise().then()" clauses
+
+## Rule Details
+
+Examples of **incorrect** code for this rule:
+
+```js
+
+
+
+```
+
+Examples of **correct** code for this rule:
+
+```js
+
+
+
+```
---
+++ tests/lib/rules/async-contains-then-clause.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview Don&#39;t allow it.only and similar tests in mocha
+ * @author Steve Zelaznik
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/async-contains-then-clause"),
+  RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module"
+  }
+});
+
+var ruleTester = new RuleTester();
+ruleTester.run("async-contains-then-clause", rule, {
+  valid: [
+  ],
+
+  invalid: [
+  ]
+});
